b8f8dbbedb89cdc2171626e456dde42f7b8aeaf5
shaheryar <shaheryartariq909@gmail.com>
2020-04-29 12:57:59 +0500

    1. Added SORT Detector 2. Commented video write function to save cost

--- Utils/ImageUtitls.py
+++ Utils/ImageUtitls.py
@@ -7,8 +7,27 @@ def drawBoxes(image,rect,box_color,caption):
     image = cv2.rectangle(image, (x1, y1), (x2, y2), box_color, 4)
     # image = cv2.rectangle(image, (x1, y1 - 30), (x1 + 200, y1), box_color, cv2.FILLED)
     font = cv2.FONT_HERSHEY_PLAIN
-    image = cv2.putText(image, caption, (x1, y1 - 15), font, 1.2, (0, 0, 0), 2)
+    image = cv2.putText(image, caption, (x1, y1 - 15), font, 1.2, box_color, 2)
     return image
+
+def IoU(boxA, boxB):
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    # return the intersection over union value
+    return iou
 
 
 
